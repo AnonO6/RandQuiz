979918164502c68df2433ebf19008f8f53397135
aviral shukla <aviralshukla@Mani.local>
2024-03-04 21:22:51 +0530

    Fixed more bugs class 4/03/24

--- app.js
+++ app.js
@@ -3,54 +3,39 @@ var quizData = {
   Question: [
     {
       question: "Which of the following is NOT a primary color?",
-      a: "Red",
-      b: "Green",
-      c: "Yellow",
-      d: "Blue",
+      options: ["Red", "Green", "Yellow", "Blue"],
       ans: "b",
     },
     {
       question: "Which continent is the largest in terms of land area?",
-      a: "Europe",
-      b: "Asia",
-      c: "Afria",
-      d: "North Americal",
+      options: ["Europe", "Asia", "Afria", "North Americal"],
       ans: "b",
     },
     {
       question: "What is the chemical symbol for gold?",
-      a: "Au",
-      b: "Ag",
-      c: "Cu",
-      d: "Fe",
+      options: ["Au", "Ag", "Cu", "Fe"],
       ans: "a",
     },
     {
       question: "What is the capital of Australia?",
-      a: "Sydney",
-      b: "Gabba",
-      c: "Canberra",
-      d: "Melborne",
+      options: ["Sydney", "Gabba", "Canberra", "Melborne"],
       ans: "c",
     },
     {
       question: "What is the author of the Harry Potter?",
-      a: "Ruskin Bonk",
-      b: "JK Rowling",
-      c: "Percy Jackson",
-      d: "Franz Kafka",
+      options: ["Ruskin Bonk", "JK Rowling", "Percy Jackson", "Franz Kafka"],
       ans: "b",
     },
   ],
 };
-var state = false; //To know whether name is entered or not
+var state = 0; //To know whether name is entered or not
 function saveName() {
   let person = prompt("Enter name to play Quiz", "default");
 
   if (person != null) {
     document.getElementById("demo").innerHTML =
       "Hello " + person + ", Lets begin with the quiz";
-    state = true;
+    state++;
   }
 }
 function checkAns(uans, num) {
@@ -58,43 +43,30 @@ function checkAns(uans, num) {
     alert("HOORAAYYY!!");
   }
 }
+function editButton(btn, num, option) {
+  btn.innerHTML =
+    option + ")" + quizData.Question[num].options[option.charCodeAt(0) - 97];
+  btn.class = "op_class";
+  btn.onclick = () => {
+    checkAns(option, num);
+  };
+  return btn;
+}
 function randQuiz() {
-  if (state) {
+  if (state == 1) {
+    var btn1 = document.createElement("BUTTON");
+    var btn2 = document.createElement("BUTTON");
+    var btn3 = document.createElement("BUTTON");
+    var btn4 = document.createElement("BUTTON");
+    state++;
+  }
+  if (state > 1) {
     var num = Math.floor(Math.random() * 5);
     document.getElementById("ques").innerHTML =
       "Q." + quizData.Question[num].question;
-    //
-    const btn1 = document.createElement("BUTTON");
-    var option; //To accepts the user's answer
-    btn1.innerHTML = "a)" + quizData.Question[num].a;
-    btn1.class = "op_class";
-    btn1.onclick = () => {
-      checkAns("a", num);
-    };
-    //
-    const btn2 = document.createElement("BUTTON");
-    btn2.innerHTML = "b)" + quizData.Question[num].b;
-    btn2.class = "op_class";
-    btn2.onclick = () => {
-      checkAns("b", num);
-    };
-    //
-    const btn3 = document.createElement("BUTTON");
-    btn3.innerHTML = "c)" + quizData.Question[num].c;
-    btn3.class = "op_class";
-    btn3.onclick = () => {
-      checkAns("c", num);
-    };
-    //
-    const btn4 = document.createElement("BUTTON");
-    btn4.innerHTML = "d)" + quizData.Question[num].d;
-    btn4.class = "op_class";
-    btn4.onclick = () => {
-      checkAns("d", num);
-    };
-    document.getElementById("qdiv").appendChild(btn1);
-    document.getElementById("qdiv").appendChild(btn2);
-    document.getElementById("qdiv").appendChild(btn3);
-    document.getElementById("qdiv").appendChild(btn4);
+    document.getElementById("qdiv").appendChild(editButton(btn1, num, "a"));
+    document.getElementById("qdiv").appendChild(editButton(btn2, num, "b"));
+    document.getElementById("qdiv").appendChild(editButton(btn3, num, "c"));
+    document.getElementById("qdiv").appendChild(editButton(btn4, num, "d"));
   }
 }
